63456d42666df2fafb68f1c43b399422a24aaf68
Stephen Tivenan <stivenan@mail.umw.edu>
2019-02-01 14:58:46 -0500

    I am finished with the calculator

--- IntroToProgramming/Calculator_Tivenan.py
+++ IntroToProgramming/Calculator_Tivenan.py
@@ -6,9 +6,9 @@ Created on Tue Jan 29 13:56:22 2019
 @author: StephenTivenan
 """
 emptylist2=[]
-x=55 ##Input variable x
-y=30 ##Input variable y
-z=100 ##Input variable z
+x=2 ##Input variable x
+y=1 ##Input variable y
+z=1 ##Input variable z
 ##Start of operations
 
 ##Operation1
@@ -31,22 +31,12 @@ emptylist2.append(d)
 e=7%3
 emptylist2.append(e)
 
-print(emptylist2)
-print('This is the Original Set without Modifications')
 
 emptylist2[2]=emptylist2[2]+3
-print('This is the 3rd element +3')
 
-print(emptylist2[2])
-print('This is the Set with Modifications')
 
-print(emptylist2)
 emptylist2[4]=emptylist2[4]*(3/4)
-print('This is the 5th element multiplied 3/4')
-print(emptylist2[4])
 
-print(emptylist2)
-print('This is the Set with Modifications')
 
-print(emptylist2[0]+emptylist2[1]+emptylist2[2]+emptylist2[3]+emptylist2[4])
-print('This is the Sum of the Set')
+print( 'This is the Sum of the Components:', emptylist2[0]+emptylist2[1]+emptylist2[2]+emptylist2[3]+emptylist2[4])
+
